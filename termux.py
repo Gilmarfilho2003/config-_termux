9e4ebb127576008bc7415ca4cf1614dc0240ea70
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-11-01 12:04:12 -0300

    Update termux.py

--- termux.py
+++ termux.py
@@ -23,12 +23,6 @@ do = input('''
 if do == '1':
   
   print ("autualizando repositorio do termux")
-os.system ("apt-get update -y")
-os.system ("apt-get upgrade -y")
-
-
-if do == '2:
-print ("instalando python  2 3 e autualinzando")
-os.system ("pkg install python2 -y")
-os.system ("pkg install python3 -y")
+os.system ("apt-get update ")
+os.system ("apt-get upgrade ")
 
